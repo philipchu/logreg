b19ec8b415d454e2a3edd20c83423c0b09e24c40
philipchu <phillychu@gmail.com>
2021-09-09 21:36:29 -0700

    test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import log_reg_becker
 import numpy as np
 import timeit
 
+testing
+
 simulated_separableish_features, simulated_labels = log_reg_becker.gen_data()
 beck_rt = timeit.default_timer()
 log_reg_becker.run_preds(simulated_separableish_features, simulated_labels, num_steps=100)
